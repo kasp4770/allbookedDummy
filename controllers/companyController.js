9049812837bae93321f5ec0a0f28c5f642faf830
kasp4770 <kasp4770@edu.easj.dk>
2019-10-02 17:24:24 +0200

    Korrekt JSON struktur i list.res.json

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,33 +1,45 @@
 const db = require('../config/database');
 
-
-
-//IKKE FÆRDIG: Skal have lavet den rette JSON struktur
 exports.list = (req, res) => {
-
     db.companies.findAll({
         include: [{ model: db.amenities, raw: true }]
-    })/*.then(companies => {
-
-        obj = companies.forEach(element => {
-            
-        });
-
-    })*/
-
-
-
-
-    /*.then( companies => {
-        //companies.Amenities.map()
-        return companies.map( com => com.Amenities);
-
+    })
+    //Omstrukturerer JSON således at amenity-array'et består af strings istedet for objekter
+    .then(companies => {
         
-    })*/.then(result => {
+        var newCompanyList = [];
+
+        for(let i = 0; i < companies.length; i++){
+
+            var newAmenityList = [];
+
+            for(let j = 0; j < companies[i].Amenities.length; j++){
+                newAmenityList.push(companies[i].Amenities[j].name);
+            }
+            var newCompanyObj = {
+                id: companies[i].id,
+                name: companies[i].name,
+                address: companies[i].address,
+                zip: companies[i].zip,
+                city: companies[i].city,
+                latitudeY: companies[i].latitudeY,
+                longitudeX: companies[i].longitudeX,
+                accommodationType: companies[i].accommodationType,
+                companyStars: companies[i].companyStars,
+                companyImage: companies[i].companyImage,
+                lowestPrice: companies[i].lowestPrice,
+                rating: companies[i].rating,
+                noOfReviews: companies[i].noOfReviews,
+                amenities: newAmenityList
+            };
+
+            newCompanyList.push(newCompanyObj)
+        }
+        return newCompanyList;
+    })
+    .then(result => {
         res.json(result);
     })
-
-
 }
 
 
