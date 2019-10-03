ce1ca4d4ca3ea710f8a02a2f9130be0025bbd77c
kasp4770 <kasp4770@edu.easj.dk>
2019-10-03 12:57:30 +0200

    lavet metode 'companyListToPreferedStructure()' for at undgå duplikeret kode

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -3,44 +3,40 @@ const db = require('../config/database');
 exports.list = (req, res) => {
     db.companies.findAll({
         include: [{ model: db.amenities, raw: true }]
-    })
-    //Omstrukturerer JSON således at amenity-array'et består af strings istedet for objekter
-    .then(companies => {
-        
-        var newCompanyList = [];
-
-        for(let i = 0; i < companies.length; i++){
-
-            var newAmenityList = [];
-
-            for(let j = 0; j < companies[i].Amenities.length; j++){
-                newAmenityList.push(companies[i].Amenities[j].name);
-            }
-            var newCompanyObj = {
-                id: companies[i].id,
-                name: companies[i].name,
-                address: companies[i].address,
-                zip: companies[i].zip,
-                city: companies[i].city,
-                latitudeY: companies[i].latitudeY,
-                longitudeX: companies[i].longitudeX,
-                accommodationType: companies[i].accommodationType,
-                companyStars: companies[i].companyStars,
-                companyImage: companies[i].companyImage,
-                lowestPrice: companies[i].lowestPrice,
-                rating: companies[i].rating,
-                noOfReviews: companies[i].noOfReviews,
-                amenities: newAmenityList
-            };
-
-            newCompanyList.push(newCompanyObj)
-        }
-        return newCompanyList;
-    })
-    .then(result => {
-        res.json(result);
+    }).then(companies => {
+        res.json(companyListToPreferedStructure(companies));
     })
 }
 
+//Omstrukturerer JSON således at amenity-array'et består af strings istedet for objekter
+function companyListToPreferedStructure(list) {
+    var newCompanyList = [];
 
+    for (let i = 0; i < list.length; i++) {
 
+        var newAmenityList = [];
+
+        for (let j = 0; j < list[i].Amenities.length; j++) {
+            newAmenityList.push(list[i].Amenities[j].name);
+        }
+        var newCompanyObj = {
+            id: list[i].id,
+            name: list[i].name,
+            address: list[i].address,
+            zip: list[i].zip,
+            city: list[i].city,
+            latitudeY: list[i].latitudeY,
+            longitudeX: list[i].longitudeX,
+            accommodationType: list[i].accommodationType,
+            companyStars: list[i].companyStars,
+            companyImage: list[i].companyImage,
+            lowestPrice: list[i].lowestPrice,
+            rating: list[i].rating,
+            noOfReviews: list[i].noOfReviews,
+            amenities: newAmenityList
+        };
+
+        newCompanyList.push(newCompanyObj)
+    }
+    return newCompanyList;
+}
